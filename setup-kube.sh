b476ec808dd6ca0ed6ac031cc55c3871df58d97f
Lee TaeWoo <syr@meu.works>
2022-02-17 19:30:53 +0900

    update setup-kube.sh

--- setup-kube.sh
+++ setup-kube.sh
@@ -3,8 +3,7 @@ SERVICE_ACCOUNT=/var/run/secrets/kubernetes.io/serviceaccount
 kubectl config set-cluster cluster \
     --server=https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT \
     --embed-certs \
-    --certificate-authority=$SERVICE_ACCOUNT/ca.crt \
-    --namespace=$(cat $SERVICE_ACCOUNT/namespace)
+    --certificate-authority=$SERVICE_ACCOUNT/ca.crt
 
 kubectl config set-credentials user \
     --token=$(cat $SERVICE_ACCOUNT/token)
