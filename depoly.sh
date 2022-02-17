bc37df2fbad81e1d494d4374da126800e0ffb37d
Lee TaeWoo <syr@meu.works>
2022-02-17 13:30:15 +0900

    dynamic mkdir runner directory

--- depoly.sh
+++ depoly.sh
@@ -1,3 +1,4 @@
-kubectl apply -f secret.yml &&
+mkdir -p runner &&
+    kubectl apply -f secret.yml &&
     kubectl apply -f github-runner.yml &&
     kubectl rollout restart deployment/madome-runner
